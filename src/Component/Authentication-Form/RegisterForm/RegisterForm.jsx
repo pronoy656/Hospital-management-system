4f04f59627b3c4d0b5690483d14450e53c3f2041
pronoy656 <pronoypaul56@gmail.com>
2025-06-21 23:59:44 +0600

    input fill add in register form

--- src/Component/Authentication-Form/RegisterForm/RegisterForm.jsx
+++ src/Component/Authentication-Form/RegisterForm/RegisterForm.jsx
@@ -67,7 +67,19 @@ const RegisterForm = () => {
 
             <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
           </div>
-
+          <div className="mt-4">
+            <label
+              className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+              htmlFor="LoggingEmailAddress"
+            >
+              Email Address
+            </label>
+            <input
+              id="LoggingEmailAddress"
+              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+              type="email"
+            />
+          </div>
           <div className="mt-4">
             <label
               className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
